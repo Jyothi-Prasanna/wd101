eec1e3d6a32d82903c75f76a405a34b36937573c
Jyothi-Prasanna <133939905+Jyothi-Prasanna@users.noreply.github.com>
2023-11-21 19:30:38 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,49 +1,65 @@
-document.addEventListener('DOMContentLoaded', function () {
-    const registrationForm = document.getElementById('registrationForm');
-    const dataTableBody = document.querySelector('#dataTable tbody');
+document.addEventListener('DOMContentLoaded', () => {
+    const form = document.getElementById('registrationForm');
+    const tableBody = document.querySelector('#entriesTable tbody');
 
-    registrationForm.addEventListener('submit', function (event) {
+    form.addEventListener('submit', function (event) {
         event.preventDefault();
 
         const name = document.getElementById('name').value;
         const email = document.getElementById('email').value;
         const password = document.getElementById('password').value;
         const dob = document.getElementById('dob').value;
-        const termsAccepted = document.getElementById('terms').checked;
+        const terms = document.getElementById('terms').checked;
 
-        if (validateDob(dob) && termsAccepted) {
-            addRowToTable(name, email, password, dob, termsAccepted);
-            saveDataToLocalStorage();
-            registrationForm.reset();
-        } else {
-            alert('Please enter a valid Date of Birth and accept the terms.');
+        if (!isValidDateOfBirth(dob)) {
+            alert('Please enter a valid date of birth between 18 and 55 years.');
+            return;
         }
+
+        const entry = { name, email, password, dob, terms };
+
+        addEntry(entry);
+        saveToLocalStorage();
+
+        this.reset();
     });
 
-    function validateDob(dob) {
-        // Add your logic to validate date of birth (between 18 and 55 years old)
-        // Return true if valid, false otherwise
-        return true;
+    function isValidDateOfBirth(dob) {
+        const currentDate = new Date();
+        const inputDate = new Date(dob);
+        const age = currentDate.getFullYear() - inputDate.getFullYear();
+        const monthDiff = currentDate.getMonth() - inputDate.getMonth();
+
+        return age > 18 && age < 55 && (monthDiff > 0 || (monthDiff === 0 && currentDate.getDate() >= inputDate.getDate()));
     }
 
-    function addRowToTable(name, email, password, dob, termsAccepted) {
-        const newRow = dataTableBody.insertRow();
-        const cells = [
-            newRow.insertCell(0),
-            newRow.insertCell(1),
-            newRow.insertCell(2),
-            newRow.insertCell(3),
-            newRow.insertCell(4)
-        ];
-
-        cells[0].textContent = name;
-        cells[1].textContent = email;
-        cells[2].textContent = password;
-        cells[3].textContent = dob;
-        cells[4].textContent = termsAccepted ? 'Yes' : 'No';
+    function addEntry(entry) {
+        const row = tableBody.insertRow();
+        for (const key in entry) {
+            if (entry.hasOwnProperty(key)) {
+                const cell = row.insertCell();
+                cell.textContent = entry[key];
+            }
+        }
     }
 
-    function saveDataToLocalStorage() {
-        // Add your logic to save data to local storage
+    function saveToLocalStorage() {
+        const entries = Array.from(tableBody.rows).map(row => {
+            const entry = {};
+            for (let i = 0; i < row.cells.length; i++) {
+                entry[tableBody.rows[0].cells[i].textContent.toLowerCase()] = row.cells[i].textContent;
+            }
+            return entry;
+        });
+
+        localStorage.setItem('entries', JSON.stringify(entries));
     }
+
+    function loadFromLocalStorage() {
+        const entries = JSON.parse(localStorage.getItem('entries')) || [];
+
+        entries.forEach(entry => addEntry(entry));
+    }
+
+    loadFromLocalStorage();
 });
